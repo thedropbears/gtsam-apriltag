95fb8d9db5b0b21172193f40878cb0f6947b3c7d
James Ward <james@thedropbears.org.au>
2026-09-23 14:22:53 +1000

    Add constructor for estimator

--- gtsam_apriltag/include/estimator.hpp
+++ gtsam_apriltag/include/estimator.hpp
@@ -1,15 +1,33 @@
+/*
+ * Copyright 2026, The Drop Bears - FRC Team 4774
+ * Copyright 2026, The Warriors of East Harlem - FRC Team 1880
+ *
+ * Redistribution and use in source and binary forms, with or without modification, are permitted
+ * provided that the following conditions are met:
+ *
+ * 1. Redistributions of source code must retain the above copyright notice, this list of conditions
+ * and the following disclaimer.
+ *
+ * 2. Redistributions in binary form must reproduce the above copyright notice, this list of
+ * conditions and the following disclaimer in the documentation and/or other materials provided with
+ * the distribution.
+ *
+ * 3. Neither the name of the copyright holder nor the names of its contributors may be used to
+ * endorse or promote products derived from this software without specific prior written permission.
+ */
+
 #pragma once
 
-#include <gtsam/nonlinear/ISAM2.h>
+#include <gtsam/nonlinear/IncrementalFixedLagSmoother.h>
 
 namespace gtsam_apriltag
 {
 class Estimator
 {
 public:
-  auto foo() -> int {return 42;};
+  Estimator();
 
 private:
-  gtsam::ISAM2 isam_;
+  gtsam::IncrementalFixedLagSmoother smoother_;
 };
 }  // namespace gtsam_apriltag
